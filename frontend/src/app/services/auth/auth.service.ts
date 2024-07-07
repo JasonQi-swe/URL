5a256cc55b3aa0e67ae559fd99e561209c8ba85b
ZijianQiMac <7zijian@gmail.com>
2024-07-07 23:12:45 +0200

    UI: finished a basic login 2

---
+++ frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/api'; 
+  private authState = new BehaviorSubject<boolean>(this.hasToken());
+
+  constructor(private http: HttpClient) {}
+
+  hasToken(): boolean {
+    return !!localStorage.getItem('accessToken');
+  }
+
+  getAuthState(): Observable<boolean> {
+    return this.authState.asObservable();
+  }
+
+  login(loginRequest: any): Observable<any> {
+    return new Observable(observer => {
+      this.http.post(`${this.apiUrl}/login`, loginRequest).subscribe(
+        (response: any) => {
+          localStorage.setItem('accessToken', response.accessToken);
+          localStorage.setItem('refreshToken', response.refreshToken);
+          this.authState.next(true);
+          observer.next(response);
+          observer.complete();
+        },
+        (error) => {
+          observer.error(error);
+        }
+      );
+    });
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    this.authState.next(false);
+  }
+}
